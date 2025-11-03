631b558bf1d01febc285e2eeecf73ceb82692258
c04o <c04o@proton.me>
2025-11-03 17:52:54 -0600

    Agregar propiedades precio, fotos y observaciones a Producto

--- src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
+++ src/main/java/ni/edu/uam/facturacion/modelo/Producto.java
@@ -4,6 +4,8 @@ import javax.persistence.*;
 import org.openxava.annotations.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Entity @Getter @Setter
 public class Producto {
 
@@ -12,6 +14,16 @@ public class Producto {
 
     @Column(length=50) @Required
     String descripcion;
+
+    @Money // La propiedad precio se usa para almacenar dinero
+    BigDecimal precio; // BigDecimal se suele usar para dinero
+
+    @Files // Una galería de fotos completa está disponible
+    @Column(length=32) // La cadena de 32 de longitud es para almacenar la clave de la galería
+    String fotos;
+
+    @TextArea // Esto es para un texto grande, se usará un área de texto o equivalente
+    String observaciones;
 
     @ManyToOne( // La referencia se almacena como una relación en la base de datos
             fetch=FetchType.LAZY, // La referencia se carga bajo demanda
